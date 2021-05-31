3ba99ffcca8b1c38aeed8f47fa181d2d838776f4
Naysan Saran <naysan.saran@cannforecast.com>
2021-05-31 18:40:26 -0400

    Initial commit

---
+++ 01_simplest/js/simple_tree.js
@@ -0,0 +1,17 @@
+var data = {
+    "id"    : 1,
+    "title" : "A",
+    "value" : 1.1,
+    "children": [
+        {
+            "id"    : 2,
+            "title" : "B",
+            "value" : 2.1
+        },
+        {
+            "id"    : 3,
+            "title" : "C",
+            "value" : 2.2
+        }
+    ]
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# dynamic-javascript-trees
+
+Examples of dynamic javascript trees using d3-mitch-tree
+
+## Description
+
+An in-depth paragraph about your project and overview of use.
+
+## Getting Started
+
+### Base JavaScript Library
+
+* https://d3-mitch-tree.netlify.app
+
