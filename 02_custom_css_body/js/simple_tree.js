c99bd5872313fb2b6f8dbf10d935a611a86ac1d2
Naysan Saran <naysan.saran@cannforecast.com>
2021-07-29 18:55:14 -0400

    Added 02

---
+++ 02_custom_css_body/js/simple_tree.js
@@ -0,0 +1,18 @@
+var data = {
+    "id"    : 1,
+    "title" : "grades",
+    "children": [
+        {
+            "id"    : 2,
+            "title" : "assignment1",
+            "grade" : 63,
+            "max"   : 100,
+        },
+        {
+            "id"    : 3,
+            "title" : "assignment2",
+            "grade" : 95,
+            "max"   : 100,
+        }
+    ]
+};
